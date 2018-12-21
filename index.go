b9deabd52be7a46ff46db8b614245c510f1e1fe8
Snuggs <rashaunstovall@gmail.com>
2018-12-21 16:07:32 -0500

    Add index.go

---
+++ index.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt";
+  "time";
+)
+
+func main () {
+  fmt.Println (`SHIT THIS WORKS!!!`)
+
+  microphone := map[string]string{
+    "name": "Sennheiser MKH416",
+    "polar-pattern": "Figure 8 Cardioid",
+  }
+
+  Microphone := map[string]interface{}{
+    "ID": 416,
+    "Name": "Sennheiser MKH416",
+    // https://github.com/fxtlabs/date/blob/master/date.go#L93
+    "Production-Date": time.Date(1979, time.November, 13, 23, 45, 2, 0, time.UTC),
+  }
+
+  for key, value := range microphone {
+    fmt.Println(key, value)
+  }
+
+  fmt.Println("Microphone ID", Microphone["ID"])
+  fmt.Println("Microphone Name", Microphone["Name"])
+  fmt.Println("Microphone Production Date", Microphone["Production-Date"])
+}
