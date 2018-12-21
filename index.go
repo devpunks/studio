4ca473a1c55ffdc0509a1e63beea1416c182c73f
Snuggs <rashaunstovall@gmail.com>
2018-12-21 16:13:48 -0500

    Fix struct instance properties

--- index.go
+++ index.go
@@ -20,9 +20,10 @@ func main () {
     "polar-pattern": "Figure 8 Cardioid",
   }
 
-  Microphone := map[string]interface{}{
-    "ID": 416,
-    "Name": "Sennheiser MKH416",
+//Microphone := map[string]interface{}{
+  m := Microphone{
+    ID: 416,
+    Name: "Sennheiser MKH416",
     // https://github.com/fxtlabs/date/blob/master/date.go#L93
     "Production-Date": time.Date(1979, time.November, 13, 23, 45, 2, 0, time.UTC),
   }
