e13868da86d89f1a8a21d2493a221d0f75c7f9ad
Snuggs <rashaunstovall@gmail.com>
2019-11-15 18:37:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.warn ('WTF this worked!')
+const
+  worker
+, first  = document.querySelector `#first`
+, second = document.querySelector `#second`
+, result = document.querySelector `#result`
+
+let
+  worker =
+    'Worker' in window && new Worker ('/worker.js')
+
+worker.onmessage = e => {
+  result.textContent = e.data
+  console.log ('Message received from worker')
+}
+
+first.addEventHandler ('change', e => {
+  'postMessage' in worker
+    && worker.postMessage ( [first.value, second.value] )
+
+  console.log ('Message posted to worker', e)
+})
