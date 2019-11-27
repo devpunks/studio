fe1c67b001096d0eff5e2373ad16cdbeff16b469
Snuggs <rashaunstovall@gmail.com>
2019-11-27 09:02:57 -0500

    refactor index.js

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const
-  first  = document.querySelector `#first`
-, second = document.querySelector `#second`
-, result = document.querySelector `#result`
+var
+  first  = document.querySelector ('#first')
+, second = document.querySelector ('#second')
+, result = document.querySelector ('#result')
+
+, worker =
+    'Worker' in window && new Worker ('/worker.js')
 
 // PAssive event listener for touch event
 // https://developers.google.com/web/tools/lighthouse/audits/passive-event-listeners?utm_source=lighthouse&utm_medium=devtools
 
-let
-  worker =
-    'Worker' in window && new Worker ('/worker.js')
+document.addEventListener
+  ('touchstart', function () {}, { passive: true })
 
-worker.onmessage = e => {
+
+worker.onmessage = function (e) {
   result.textContent = e.data
   console.log ('Message received from worker')
 }
 
 first
-&& first.addEventHandler ('change', e => {
+&& first.addEventHandler ('change', function (e) {
   'postMessage' in worker
     && worker.postMessage ( [first.value, second.value] )
 
