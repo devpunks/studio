600de2de59eb5aa39138e02772a71ca6afd8262e
Snuggs <rashaunstovall@gmail.com>
2019-11-30 20:40:10 -0500

    Update worker location

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var
 , result = document.querySelector ('#result')
 
 , worker =
-    'Worker' in window && new Worker ('/worker.js')
+    'Worker' in window && new Worker ('worker.js')
 
 // Passive event listener for touch event
 // https://developers.google.com/web/tools/lighthouse/audits/passive-event-listeners?utm_source=lighthouse&utm_medium=devtools
