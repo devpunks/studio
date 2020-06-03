de16cea25827ae87fb047a73474d6583e969e274
Snuggs <rashaunstovall@gmail.com>
2020-06-03 11:05:07 -0400

    Add Vibrate API

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 // Push API - https://developer.mozilla.org/en-US/docs/Web/API/Push_API
 // Clipboard API - https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
 // sendBeacon - https://developer.mozilla.org/en-US/docs/Web/API/Navigator/sendBeacon
+// Vibrate API - https://developer.mozilla.org/en-US/docs/Web/API/Navigator/vibrate
 
 var
   first  = document.querySelector ('#first')
