d450b53f9e026e834c74cdcdad60a3b3111e5132
snuggs <rashaunstovall@gmail.com>
2022-03-16 11:21:56 -0400

    Setup pm2 and convert app to module

--- .config/pm2.js
+++ .config/pm2.js
@@ -1,3 +1,14 @@
-export {
-  apps: [ "studio" ],
-};
+export default {
+  apps: [
+    {
+      name: 'Studio',
+      script: 'node .',
+      watch:
+        [ '.' ],
+      ignore_watch:
+        [ 'node_modules' ],
+      env:
+        { NODE_ENV: 'development' }
+    },
+  ]
+}
