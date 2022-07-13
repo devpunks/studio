56d3ebca3df270241d9168ca64cf8d44b5d66874
snuggs <rashaunstovall@gmail.com>
2022-07-13 10:26:48 -0400

    Add express example

--- index.es.js
+++ index.es.js
@@ -1,5 +1,29 @@
+import express from 'express'
 import { METHODS } from "http"
 
 console.log(METHODS)
 
 export default class { }
+
+// Require express and create an instance of it
+var app = express();
+
+// on the request to root (localhost:3000/)
+app.get('/', function (req, res) {
+    res.send('<b>My</b> first express http server');
+});
+
+// On localhost:3000/welcome
+app.get('/welcome', function (req, res) {
+    res.send('<b>Hello</b> welcome to my http server made with express');
+});
+
+// Change the 404 message modifing the middleware
+app.use(function(req, res, next) {
+    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
+});
+
+// start the server in the port 3000 !
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000.');
+});
