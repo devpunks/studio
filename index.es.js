90af3e59c39d44367ee4dcaad038c13ec8a98b1b
snuggs <rashaunstovall@gmail.com>
2022-07-21 08:40:48 -0400

    Update index.js

--- index.es.js
+++ index.es.js
@@ -1,30 +1,7 @@
-import express from 'express'
 import { METHODS } from "http"
+import { Router, default as express } from 'express'
 
 console.log(METHODS)
-
-export default class { }
-
-// Require express and create an instance of it
-const app = express();
-
-// on the request to root (localhost:3000/)
-app.get('/', function (req, res) {
-	// res.json(JSON.stringify({ foo: 'bar' }));
-	res.json({
-	data: 'SHEESH'
-	})
-});
-
-// On localhost:3000/welcome
-app.get('/welcome', function (req, res) {
-	res.send('<b>Hello</b> welcome to my http server made with express');
-});
-
-// Change the 404 message modifing the middleware
-app.use(function(req, res, next) {
-	res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
-});
 
 const PORT = process.env.PORT || 3000
 
