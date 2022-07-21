fca6976680a07e0981a88567d7913169297776c0
snuggs <rashaunstovall@gmail.com>
2022-07-21 08:40:57 -0400

    Update Router class

--- index.es.js
+++ index.es.js
@@ -5,6 +5,19 @@ console.log(METHODS)
 
 const PORT = process.env.PORT || 3000
 
+export default class extends Router{
+  constructor() {
+    console.warn(this)
+  }
+}
+
+const app = new Router
+  .get('/', function (req, res) {
+    res.json({ data: 'SHEESH' }) })
+  .get('/welcome', function (req, res) {
+    res.send('<h1>Hello</h1> welcome to my http server made with express') })
+  .use(function(req, res, next) {
+    res.status(404).send("That route doesn't exist"); });
 app.listen(PORT, function () {
 	console.log(`Example app listening on port ${PORT}`);
 });
