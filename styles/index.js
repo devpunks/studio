af4e3d387fa37169190e6aa317a8625cf6f44415
snuggs <rashaunstovall@gmail.com>
2023-01-15 14:54:20 -0500

    Add postcss script

---
+++ styles/index.js
@@ -0,0 +1,22 @@
+// dependencies
+import fs from 'fs'
+import postcss from 'postcss'
+import atImport from 'postcss-import'
+
+// css to be processed
+const css = fs.readFileSync("./index.css", "utf8")
+
+console.log(css)
+
+// process css
+postcss()
+  .use(atImport())
+  .process(css, {
+    // `from` option is needed here
+    from: "./index.css"
+  })
+  .then((result) => {
+    const output = result.css
+
+    console.log(output)
+  })
