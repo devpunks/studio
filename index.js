cac31744ff66ad9f2c63561bc743cd31de868421
snuggs <rashaunstovall@gmail.com>
2024-09-13 19:07:46 -0400

    Remove www

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
     - https://quirksmode.org/blog/archives/2017/12/native_form_val.html
     - https://developer.mozilla.org/en-US/docs/Web/HTML/Constraint_validation
     - https://quirksmode.org/blog/archives/2017/12/native_form_val.html
-    - https://www.quirksmode.org/blog/archives/2017/12/native_form_val_1.html
+    - https://quirksmode.org/blog/archives/2017/12/native_form_val_1.html
     - https://www.quirksmode.org/blog/archives/2017/12/native_form_val_2.html
 */
 var
