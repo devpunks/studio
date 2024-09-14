b4c97a398647785e06654094ed3f590e43d9e349
snuggs <rashaunstovall@gmail.com>
2024-09-14 19:59:54 -0400

    Fix whitespace

--- index.js
+++ index.js
@@ -18,6 +18,7 @@
     - https://quirksmode.org/blog/archives/2017/12/native_form_val_1.html
     - https://quirksmode.org/blog/archives/2017/12/native_form_val_2.html
 */
+
 var
   first  = document.querySelector ('#first')
 , second = document.querySelector ('#second')
