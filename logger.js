83ab3aae80321b4800e9013c8ac9a76582ccd1ca
snuggs <rashaunstovall@gmail.com>
2025-11-30 04:52:07 -0800

    Remove www

--- logger.js
+++ logger.js
@@ -1,7 +1,7 @@
 // https://youtube.com/watch?v=lWmbK6iZML4
 // Logging libraries - https://betterstack.com/community/guides/logging/best-nodejs-logging-libraries
 // Pino vs Winston - https://betterstack.com/community/comparisons/pino-vs-winston
-// Structured logging in Node.js with Winston - https://www.thedreaming.org/2020/06/24/structured-logging-nodejs
+// Structured logging in Node.js with Winston - https://thedreaming.org/2020/06/24/structured-logging-nodejs
 console.log ('LOGGERRRRRRRRRRRRRRRR')
 
 class Logger {
