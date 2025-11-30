a16344bc69d52f52849d26dc13dd9bccf07ad93d
snuggs <rashaunstovall@gmail.com>
2025-11-30 04:48:25 -0800

    Add link for pino vs winston

--- logger.js
+++ logger.js
@@ -1,4 +1,5 @@
 // https://youtube.com/watch?v=lWmbK6iZML4
+// Pino vs Winston -https://betterstack.com/community/comparisons/pino-vs-winston
 console.log ('LOGGERRRRRRRRRRRRRRRR')
 
 export default class {
