2f93761c923ba6134e019392dbe09b4262e359a1
snuggs <rashaunstovall@gmail.com>
2025-11-30 04:57:24 -0800

    Add documentation for pino vs winston

--- logger.js
+++ logger.js
@@ -1,5 +1,7 @@
 // https://youtube.com/watch?v=lWmbK6iZML4
-// Pino vs Winston - https://betterstack.com/community/comparisons/pino-vs-winston
+// Pino vs Winston 
+//   - https://betterstack.com/community/comparisons/pino-vs-winston
+//   - https://dev.to/wallacefreitas/pino-vs-winston-choosing-the-right-logger-for-your-nodejs-application-369n
 // Logging libraries - https://betterstack.com/community/guides/logging/best-nodejs-logging-libraries
 // Structured logging in Node.js with Winston - https://thedreaming.org/2020/06/24/structured-logging-nodejs
 console.log ('LOGGERRRRRRRRRRRRRRRR')
